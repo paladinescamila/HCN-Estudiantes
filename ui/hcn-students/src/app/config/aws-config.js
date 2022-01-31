f4ba2fdf4474ac8672f1a803c31d465721f15131
paladinescamila <camilapaladines27@gmail.com>
2022-01-31 16:00:20 -0500

    Updated Amazon Cognito settings

--- ui/hcn-students/src/app/config/aws-config.js
+++ ui/hcn-students/src/app/config/aws-config.js
@@ -1,5 +1,5 @@
 export const COGNITO = {
-  REGION: "us-east-2",
-  USER_POOL_ID: "us-east-2_DElR0Xfv2",
-  APP_CLIENT_ID: "4dl4hav6qirtivnrcqefkg5kge",
+	REGION: "us-east-2",
+	USER_POOL_ID: "us-east-2_fd2tfTUBk",
+	APP_CLIENT_ID: "h2d2uehkrotkp9o3g2v1s3fcb",
 };
