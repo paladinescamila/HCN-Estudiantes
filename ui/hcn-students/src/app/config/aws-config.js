e55bf82191e13f08fd21aa9dbc579d0bcea5985b
paladinescamila <camilapaladines27@gmail.com>
2022-02-01 14:40:05 -0500

    Saved aws-config.js

--- ui/hcn-students/src/app/config/aws-config.js
+++ ui/hcn-students/src/app/config/aws-config.js
@@ -1,5 +1,5 @@
 export const COGNITO = {
 	REGION: "us-east-2",
 	USER_POOL_ID: "us-east-2_fd2tfTUBk",
-	APP_CLIENT_ID: "h2d2uehkrotkp9o3g2v1s3fcb",
+	APP_CLIENT_ID: "h2d2uehkrotkp9o3g2v1s3fcbm",
 };
